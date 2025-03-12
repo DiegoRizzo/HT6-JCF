3fa9d18e2e9d68b47287a4a0d846781be4724a12
diegorizzo <diegochunr@hotmail.com>
2025-03-11 18:03:50 -0600

    Cambios a LeerArchivo.java

--- LeerArchivo.java
+++ LeerArchivo.java
@@ -5,51 +5,34 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Arrays;
+ 
+public class LeerArchivo {
+ 
+    File archivo = new File ("pokemon_data_pokeapi.csv");
+    ArrayList<ArrayList<String>> datos;
 
-public class LeerArchivo{
-
-File archivo = new File ("pokemon_data_pokeapi.csv");
-
-ArrayList<String> datos_temporal;
-ArrayList<ArrayList<String>> datos;
-
-    public ArrayList<ArrayList<String>> leer_TXT(String nombre_archivo) {
-        datos = new ArrayList<ArrayList<String>>();
-
+    public ArrayList<ArrayList<String>> leer_csv(String nombre_archivo) {
+        
+        datos = new ArrayList<>();
+        String linea;
+        String separador = ",";
+ 
         Path filePath = Paths.get(nombre_archivo);
         try {
             BufferedReader br = Files.newBufferedReader(filePath);
-            String linea;
 
             while ((linea = br.readLine()) != null) {
-                String[] datosDeLinea = linea.split("");
-                datos_temporal = new ArrayList<String>();
-
-                for (String dato : datosDeLinea) {
-                    datos_temporal.add(dato);
-                }
-                datos.add(datos_temporal);
+                String[] datos_de_linea = linea.split(separador);
+                datos.add(new ArrayList<String>(Arrays.asList(datos_de_linea)));
             }
-        } catch (IOException exception) {
-            exception.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         System.out.println(datos);
-
+ 
         return datos;
     }
-    /**
-     * @return
-     * devuelve los datos
-     */
-    public String DevolverDatos(){
-        String mensaje = "";
-        for(ArrayList<String> mamada : datos){
-            for(String mamada2: mamada){
-                mensaje+= mamada2+" ";
-            }
-        }
-        System.out.println(mensaje);
-        return mensaje;
-    }
-
+ 
 }
+ 
